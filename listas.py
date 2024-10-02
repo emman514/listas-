ed34846a956444d74e03e63a44c74e0054ab6497
Emmanuel Castruita Soto <a.23308051281176@cbtis128.edu.mx>
2024-10-01 21:18:03 -0600

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,26 @@
+# Crear una lista de números del 1 al 5
+print("")
+print("castruita soto emmanuel")
+print("")
+numeros = [1, 2, 3, 4, 5]
+
+# Imprimir la lista
+print(numeros)  # resultado: [1, 2, 3, 4, 5]
+print("")
+# Crear una lista de frutas
+frutas = ["manzana", "banana", "cereza"]
+
+# Acceder al primer elemento de la lista
+print(frutas[0])  # resultado: manzana
+
+# Acceder al último elemento de la lista
+print(frutas[-1])  # resultado: cereza
+print("")
+# Crear una lista de días de la semana
+dias = ["lunes", "martes", "miércoles", "jueves", "viernes"]
+
+# Acceder al primer día de la lista
+print(dias[0])  # resultado: lunes
+
+# Acceder al último día de la lista
+print(dias[-1])  # resultado: viernes
